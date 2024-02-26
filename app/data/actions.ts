b4ad9fabd71c543f9ca67cd2d081981b198d25bb
damiresz <damirabd879@gmail.com>
2024-02-26 06:53:34 +0100

    add todo validate

--- app/data/actions.ts
+++ app/data/actions.ts
@@ -1,11 +1,49 @@
 "use server";
+import { z } from "zod";
 import { ITodo, TodoListArray } from "./TodoListData";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { error } from "console";
+
+export interface IErrorState  {
+  message?: string | null;
+  errors?: {
+    title?: string[];
+    description?: string[];
+  };
+};
+
+const FormSchema = z.object({
+  title: z.string().max(16, { message: "Max length 16" }),
+  description: z.string().max(52, { message: "Max length 52" }),
+  date: z
+    .string()
+    .datetime({ message: "Invalid datetime string! Must be UTC." }),
+});
+
+const CreateTodo = FormSchema.omit({ date: true });
+
+export const createTodo = (prevErrorState: IErrorState, formData: FormData) => {
+  const validatedFields = CreateTodo.safeParse({
+    title: formData.get("todoTitle"),
+    description: formData.get("todoDescription"),
+  });
+  if (!validatedFields.success) {
+    return {
+      message: "Missing Fields. Failed to Create Invoice.",
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  revalidatePath("/dashboard");
+  redirect("/dashboard");
+};
 
 export async function fetchTodoList(date: Date): Promise<ITodo[] | null> {
   const currentDate = date.getDate();
   try {
     // await new Promise((resolve) => setTimeout(resolve, 3000));
-    
+
     const sortedTodoList = TodoListArray.filter((task) => {
       const taskDate = new Date(task.date).getDate();
       return taskDate === currentDate;
@@ -16,4 +54,3 @@ export async function fetchTodoList(date: Date): Promise<ITodo[] | null> {
     return null;
   }
 }
-
--- components/dashboard/TodoList/TodoList.tsx
+++ components/dashboard/TodoList/TodoList.tsx
@@ -1,16 +1,26 @@
 import { GoPlus } from "react-icons/go";
 import styles from "./TodoList.module.scss";
 import { CiEdit } from "react-icons/ci";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useReducer, useState } from "react";
 import { StateContext } from "../Organizer/Organizer";
-import { fetchTodoList} from "@/app/data/actions";
+import { IErrorState, createTodo, fetchTodoList } from "@/app/data/actions";
 import { ITodo } from "@/app/data/TodoListData";
+import { IoClose } from "react-icons/io5";
+import { useFormState } from "react-dom";
 
 export function TodoList() {
   const { state } = useContext(StateContext);
   const [data, setData] = useState<ITodo[] | null>(null);
+  const [add, setAdd] = useState(false);
 
+  const initialformError: IErrorState = { message: null, errors: {} };
+  const [formState, setFormState] = useFormState(createTodo, initialformError);
 
+  const handleAddClick = () => {
+    setAdd((prev) => !prev);
+  };
+
+  const handleSubmitAdd = () => {};
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,26 +28,59 @@ export function TodoList() {
       setData(todoData);
     };
     fetchData();
+    setAdd((prev) => (prev = false));
   }, [state.selectedDate]);
 
   return (
     <div className={styles.todoList}>
       <div>
         <h3>Scheduled</h3>
-        <GoPlus />
+        <button onClick={() => handleAddClick()}>
+          <GoPlus />
+        </button>
         <p>View more</p>
       </div>
-      {data?.map((todo, index) => (
-        <div key={index} className={styles.listItems}>
-          <div className={styles.listItem}>
+      <div className={styles.listItems}>
+        {add && (
+          <form action={setFormState} className={styles.addItem}>
+            <div>
+              <input
+                id="todoTitle"
+                name="todoTitle"
+                type="text"
+                placeholder="Title"
+              />
+              {formState.errors?.title?.map((error) => (
+                <span>{error}</span>
+              ))}
+            </div>
+            <div>
+              <textarea
+                id="todoDescription"
+                name="todoDescription"
+                placeholder="description"
+                rows={5}
+              ></textarea>
+              {formState.errors?.description?.map((error) => (
+                <span>{error}</span>
+              ))}
+            </div>
+            <button type="submit">Add</button>
+            <button onClick={() => handleAddClick()}>
+              <IoClose />
+            </button>
+          </form>
+        )}
+        {data?.map((todo, index) => (
+          <div key={index} className={styles.listItem}>
             <div>
               <h5>{todo.name}</h5>
               <CiEdit />
             </div>
             <p>{todo.descriptions}</p>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
